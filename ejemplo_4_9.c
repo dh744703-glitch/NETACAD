321ee49fcc334ae98bd9bea1126deaedb3588a3b
dh744703-glitch <dh744703@gmail.com>
2025-10-25 05:44:59 +0000

    modulo

---
+++ ejemplo_4_9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char word[256];
+    int i;
+    scanf("%s", word);
+    int lenght = dtrlen(word)
+    for (i=0; i < length/2; i++)
+    {
+        char c = word[i];
+        word[i] = word[length - i - 1];
+        word[ length - i -1] = c;
+    }
+    printf("%s\n", word);
+    return 0;
+}
